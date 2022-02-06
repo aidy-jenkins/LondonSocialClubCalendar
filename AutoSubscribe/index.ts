b3e7b1adba3ffbfdb3275b4ae5dc1642969147a4
aidy-jenkins <git@aidyj.uk>
2022-02-06 17:26:41 +0000

    Change token client calls

--- AutoSubscribe/index.ts
+++ AutoSubscribe/index.ts
@@ -1,26 +1,42 @@
-declare module gapi.client {
-    var calendar: any;
-}
+declare var google: any;
 
 class Index {
-    private _subscribeButton = document.getElementById("subscribe");
     private static readonly lscCalendarId = "b3v3l43gdk3fg4hdfsqbashiec@group.calendar.google.com";
     private static readonly clientId = "282443687780-1seonvl8cfhvrd4mg13inlgld6pa4i8d.apps.googleusercontent.com";
+    
+    private _subscribeButton = document.getElementById("subscribe");
+    private _loginButton = document.getElementsByClassName("g_id_signin")[0];
+
+    private _apiClient = null as any;
+    private _token = null as string;
 
     constructor() {
         this._subscribeButton.addEventListener("click", e => this.subscribe());
+
+
+        this._apiClient = google.accounts.oauth2.initTokenClient({
+            client_id: Index.clientId,
+            scope: 'https://www.googleapis.com/auth/calendar',
+            callback: (tokenResponse) => { this._token = tokenResponse.access_token },
+        });
+          
+    }
+
+    private async login() {
+        //This triggers the callback of the init method - makes perfect sense
+        this._apiClient.requestAccessToken();
     }
 
     private async subscribe() {
         try {
-            let token = (window as any).__lscgoogleauthcredential as string
+            let token = this._token;
 
             if(!token) {
                 alert("Not signed in");
                 return;
             }
 
-            await fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
+            let response = await fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
                 method: "POST",
                 body: JSON.stringify({
                     id: Index.lscCalendarId
@@ -30,18 +46,8 @@ class Index {
                     "Content-Type": "application/json"
                 }
             });
-
-            // await gapi.client.load("calendar", "v3");
-            // gapi.client.setToken({access_token: token});
-            // await gapi.client.calendar.calendarList.insert({ 
-            //     id: Index.lscCalendarId
-            // });
-            // await google.calendar("v3").calendarList.insert({ 
-            //     oauth_token: token,
-            //     requestBody: {
-            //         id: Index.lscCalendarId
-            //     }
-            // });
+            if(response.status < 200 || response.status >= 300)
+                throw new TypeError(`Request failed\r\n${response.status} - ${response.statusText}\r\n`);
         }
         catch(err) {
             console.error(err);
