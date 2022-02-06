801726a29fc769805455b9eb5d2349ee913010db
aidy-jenkins <git@aidyj.uk>
2022-02-06 18:03:45 +0000

    Change JS target

--- AutoSubscribe/index.js
+++ AutoSubscribe/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class Index {
     constructor() {
         this._subscribeButton = document.getElementById("subscribe");
@@ -13,35 +22,39 @@ class Index {
             callback: (tokenResponse) => { this._token = tokenResponse.access_token; },
         });
     }
-    async login() {
-        //This triggers the callback of the init method - makes perfect sense
-        this._apiClient.requestAccessToken();
+    login() {
+        return __awaiter(this, void 0, void 0, function* () {
+            //This triggers the callback of the init method - makes perfect sense
+            this._apiClient.requestAccessToken();
+        });
     }
-    async subscribe() {
-        try {
-            let token = this._token;
-            if (!token) {
-                alert("Not signed in");
-                return;
-            }
-            let response = await fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
-                method: "POST",
-                body: JSON.stringify({
-                    id: Index.lscCalendarId
-                }),
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json"
+    subscribe() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                let token = this._token;
+                if (!token) {
+                    alert("Not signed in");
+                    return;
                 }
-            });
-            if (response.status < 200 || response.status >= 300)
-                throw new TypeError(`Request failed\r\n${response.status} - ${response.statusText}\r\n`);
-            this._status.textContent = "Calendar successfully added or already subscribed";
-        }
-        catch (err) {
-            console.error(err);
-            alert("failed");
-        }
+                let response = yield fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        id: Index.lscCalendarId
+                    }),
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    }
+                });
+                if (response.status < 200 || response.status >= 300)
+                    throw new TypeError(`Request failed\r\n${response.status} - ${response.statusText}\r\n`);
+                this._status.textContent = "Calendar successfully added or already subscribed";
+            }
+            catch (err) {
+                console.error(err);
+                alert("failed");
+            }
+        });
     }
 }
 Index.lscCalendarId = "b3v3l43gdk3fg4hdfsqbashiec@group.calendar.google.com";
